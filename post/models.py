393f092e3bad2aa15e1709e1478e0c7236b1f6c6
Rosemy91 <rosemymathew2022@gmail.com>
2022-08-31 12:42:54 +0530

    home page

--- post/models.py
+++ post/models.py
@@ -1,3 +1,13 @@
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=150)
+    
+    def __str__(self) :
+        return self.title
+    cover = models.ImageField(upload_to="media", null=True,blank=True)
+    image1 = models.ImageField(upload_to="media", null=True,blank=True)
+    image2 = models.ImageField(upload_to="media", null=True,blank=True)
+
+
--- post/views.py
+++ post/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request,'index.html')
+# def home(request):
+#     return render(request,'index.html')
 
 # Create your views here.
+from.models import Post
+
+def home(request):
+    post=Post.objects.all()
+    context={'post':post}
+    return render(request,'index.html',context)
